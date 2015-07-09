65822da53c67626dce5cd04aff311d8f9865299a
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2015-07-09 18:26:32 +0100

    fixed POST view for the csw endpoint

--- djangopycsw/views.py
+++ djangopycsw/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from django.core.urlresolvers import reverse
 from django.contrib.sites.shortcuts import get_current_site
+from lxml import etree
 from pycsw.server import Csw
 
 from .pycswsettings import build_pycsw_settings
 
-#import logging
-#
-#logging.basicConfig(level=logging.DEBUG)
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 class CswEndpoint(View):
 
     def get(self, request):
-        print("request params: {}".format(request.GET))
         pycsw_settings = build_pycsw_settings()
         server = Csw(rtconfig=pycsw_settings, env=request.META.copy(),
                      version=request.GET["version"])
@@ -23,23 +26,26 @@ class CswEndpoint(View):
                                               reverse("csw_endpoint"))
         server.requesttype = request.method
         server.kvp = self._normalize_params(request.GET)
-        print("server.iface: {}".format(server.iface))
-        print("server.kvp: {}".format(server.kvp))
         status_code, response = server.dispatch()
-        print("server.iface: {}".format(server.iface))
         return HttpResponse(response, status=status_code,
                             content_type="application/xml")
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(CswEndpoint, self).dispatch(request, *args, **kwargs)
 
     def post(self, request):
         pycsw_settings = build_pycsw_settings()
         server = Csw(rtconfig=pycsw_settings, env=request.META.copy(),
-                     version=request.POST["version"])
+                     version=self._get_post_version(request.body))
+        logger.info(request.body)
         server.request = request.body
         server.requesttype = request.method
         status_code, response = server.dispatch()
         return HttpResponse(response, status=status_code,
                             content_type="application/xml")
 
+    # TODO - Remove this method once pycsw mainlines the pending pull request
     def _normalize_params(self, query_dict):
         """
         A hack to overcome PyCSW's early normalizing of KVP args.
@@ -53,5 +59,9 @@ class CswEndpoint(View):
         for k, v in query_dict.iteritems():
             kvp[k.lower()] = v
         return kvp
+
+    def _get_post_version(self, raw_request):
+        exml = etree.fromstring(raw_request)
+        return exml.get("version")
 
 
